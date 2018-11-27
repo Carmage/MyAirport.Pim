1b421af6a96bdf50b5c9bd04e5e3762ef39cd3a1
Amaury JULIEN <amaury.julien@epita.fr>
2018-11-27 11:32:04 +0100

    TP1 finished

---
+++ MyAirport.Pim/ModelNatif/Natif.cs
@@ -0,0 +1,23 @@
+﻿using MyAirport.Pim.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyAirport.Pim.Models
+{
+    public class Natif : AbstractDefinition
+    {
+        public override BagageDefinition GetBagage(int idBagage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BagageDefinition> GetBagage(string codeIataBagage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
