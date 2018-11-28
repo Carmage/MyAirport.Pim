e99f95d43f961a6d39e9bace3118bac017a57ffd
Amaury JULIEN <amaury.julien@epita.fr>
2018-11-28 23:07:16 +0100

    updated with SQL configuration
    until SQL requests writing

---
+++ MyAirport.Pim/Models.Sql/Sql.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyAirport.Pim.Entities;
+
+namespace MyAirport.Pim.Models
+{
+    public class Sql : AbstractDefinition
+    {
+        public override BagageDefinition GetBagage(int idBagage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<BagageDefinition> GetBagage(string codeIataBagage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
