488c1cb0414f37d6e80a0568459359d29c13cd80
Amaury JULIEN <amaury.julien@epita.fr>
2018-12-11 11:21:24 +0100

    TP2 - Page 5 : OK

--- MyAirport.Pim/Models.Sql/Sql.cs
+++ MyAirport.Pim/Models.Sql/Sql.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,63 @@ namespace MyAirport.Pim.Models
 {
     public class Sql : AbstractDefinition
     {
+        //string strCnx = ConfigurationManager.ConnectionStrings["MyAirport.Pim.Settings.DbConnect"].ConnectionString;
+        string strCnx = "Data Source=DESKTOP-8COAIAC\\SQLEXPRESS;Initial Catalog=MyAirport;Integrated Security=True";
+
+        string commandGetBagageId = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION,"
+            + " b.ESCALE, b.CLASSE, b.CONTINUATION, bp.ID_PARTICULARITE, cast(iif(bp.ID_PARTICULARITE is null, 0, 1) as bit) as 'RUSH'"
+            + " from BAGAGE b"
+            + " left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE = b.ID_BAGAGE and bp.ID_PARTICULARITE = 15"
+            + " where b.id_bagage = @id_bagage";
+
+        string commandGetBagageIata = "SELECT b.ID_BAGAGE, b.CODE_IATA, b.COMPAGNIE, b.LIGNE, b.DATE_CREATION,"
+            + " b.ESCALE, b.CLASSE, b.CONTINUATION, bp.ID_PARTICULARITE, cast(iif(bp.ID_PARTICULARITE is null, 0, 1) as bit) as 'RUSH'"
+            + " from BAGAGE b"
+            + " left outer join BAGAGE_A_POUR_PARTICULARITE bp on bp.ID_BAGAGE = b.ID_BAGAGE and bp.ID_PARTICULARITE = 15"
+            + " where b.code_iata = @code_iata";
+
         public override BagageDefinition GetBagage(int idBagage)
         {
-            throw new NotImplementedException();
+            BagageDefinition bagRes = null;
+            using (SqlConnection cnx = new SqlConnection(strCnx))
+            {
+                SqlCommand cmd = new SqlCommand(commandGetBagageId, cnx);
+                cmd.Parameters.AddWithValue("@id_bagage", idBagage);
+                cnx.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                if (sdr.Read())
+                {
+                    bagRes = new BagageDefinition()
+                    {
+                        IdBagage = sdr.GetInt32(sdr.GetOrdinal("ID_BAGAGE")),
+                        CodeIata = sdr.GetString(sdr.GetOrdinal("CODE_IATA")),
+                        EnContinuation = sdr.GetBoolean(sdr.GetOrdinal("CONTINUATION"))
+                    };
+                }
+            }
+            return bagRes;
         }
 
         public override List<BagageDefinition> GetBagage(string codeIataBagage)
         {
-            throw new NotImplementedException();
+            List<BagageDefinition> listBagRes = new List<BagageDefinition>();
+            using (SqlConnection cnx = new SqlConnection(strCnx))
+            {
+                SqlCommand cmd = new SqlCommand(commandGetBagageIata, cnx);
+                cmd.Parameters.AddWithValue("@code_iata", codeIataBagage);
+                cnx.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    listBagRes.Add(new BagageDefinition()
+                    {
+                        IdBagage = sdr.GetInt32(sdr.GetOrdinal("ID_BAGAGE")),
+                        CodeIata = sdr.GetString(sdr.GetOrdinal("CODE_IATA")),
+                        EnContinuation = sdr.GetBoolean(sdr.GetOrdinal("CONTINUATION"))
+                    });
+                }
+            }
+            return listBagRes;
         }
     }
 }
