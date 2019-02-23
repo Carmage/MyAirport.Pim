9abb13473e2750180bb18b16563506ed374f51e1
SevanMelemedjian <36965894+SevanMelemedjian@users.noreply.github.com>
2019-02-23 10:11:18 +0100

    Cleanup AbstractDefinition.cs

--- MyAirport.Pim/Models.AbstractDefinition/AbstractDefinition.cs
+++ MyAirport.Pim/Models.AbstractDefinition/AbstractDefinition.cs
@@ -1,9 +1,5 @@
 ﻿using MyAirport.Pim.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyAirport.Pim.Models
 {
